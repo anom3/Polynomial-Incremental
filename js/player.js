f3ebb29bfc2153fd2182be6ef42e72a637dbe633
anom3 <97539748+anom3@users.noreply.github.com>
2022-08-10 21:35:16 +0100

    OK

---
+++ js/player.js
@@ -0,0 +1,15 @@
+var player = {
+    money: 10,
+    marketing: [],
+    artists: [],
+    lootboxes
+}
+
+var firstMarketing = {
+    cost: 10,
+    mult: 1,
+    amount: 0,
+    bought: 0
+}
+
+player.marketing.push(firstMarketing)
