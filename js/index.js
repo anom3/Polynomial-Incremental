0118983c8cc89d2c9b8fb69eeb1538b05cdef793
anom3 <97539748+anom3@users.noreply.github.com>
2023-05-26 21:51:52 +0100

    Using Vue now, I guess.

--- js/index.js
+++ js/index.js
@@ -4,6 +4,13 @@ var app = new Vue({
         player: player
     },
     methods: {
-
+        buyMarketingGenerator(i) {
+            let g = this.player.marketing[i]
+            if (g.cost > this.player.money) return
+            this.player.money -= g.cost
+            g.cost *= 1 + (i+1) * 0.25
+            g.amount += 1
+            g.bought += 1
+        }
     }
 })
